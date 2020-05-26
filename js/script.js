af73b89c2c2846aa300aeb8a92ea022738c18a54
Sara <saracorsob@gmail.com>
2020-05-26 15:14:57 +0200

    Aggiunta commenti

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-// Debug
-alert('ciao');
+// Lista Cognomi
+
+// Chiedo all'utente il cognome
+
+// Ordino i cognomi in base alla iniziale
+
+// Stampo la Lista
+
+// Dico all'utente in che posizione della lista si trova
